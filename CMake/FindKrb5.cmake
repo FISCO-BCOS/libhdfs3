9017f72d5e97d1ef59bc6c319cc1a0a9d4bf6228
cyjseagull <yujiechen_hust@163.com>
2024-11-29 22:49:08 +0800

    fix compile

--- CMake/FindKrb5.cmake
+++ CMake/FindKrb5.cmake
@@ -8,25 +8,32 @@
 find_path(KERBEROS_INCLUDE_DIRS krb5.h PATHS "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include" NO_DEFAULT_PATH)
 
 set(LIB_DIR "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/lib")
+# libkrb5_otp
+find_library(Krb5_otp_LIB_PATH NAMES krb5_otp PATHS "${LIB_DIR}" NO_DEFAULT_PATH)
+message("*** Krb5_otp_LIB_PATH: ${Krb5_otp_LIB_PATH} **")
+# libkrb5_k5tls
+find_library(Krb5_k5tls_LIB_PATH NAMES krb5_k5tls PATHS "${LIB_DIR}" NO_DEFAULT_PATH)
+message("*** Krb5_k5tls_LIB_PATH: ${Krb5_k5tls_LIB_PATH} **")
+# libkrb5support
+find_library(Krb5support_LIB_PATH NAMES krb5support PATHS "${LIB_DIR}" NO_DEFAULT_PATH)
+message("*** Krb5support_LIB_PATH: ${Krb5support_LIB_PATH} **")
 # libkrb5
 find_library(Krb5_LIB_PATH NAMES krb5 PATHS "${LIB_DIR}" NO_DEFAULT_PATH)
-list(APPEND KERBEROS_LIBRARY ${Krb5_LIB_PATH})
 message("*** Krb5_LIB_PATH: ${Krb5_LIB_PATH} **")
 
 # libk5crypto
 find_library(K5crypto_LIB_PATH NAMES k5crypto PATHS "${LIB_DIR}" NO_DEFAULT_PATH)
-list(APPEND KERBEROS_LIBRARY ${K5crypto_LIB_PATH})
 message("*** K5crypto_LIB_PATH: ${K5crypto_LIB_PATH} **")
 
 # libcom_err
 find_library(Com_err_LIB_PATH NAMES com_err PATHS "${LIB_DIR}" NO_DEFAULT_PATH)
-list(APPEND KERBEROS_LIBRARY ${Com_err_LIB_PATH})
 message("*** Com_err_LIB_PATH: ${Com_err_LIB_PATH} **")
 
-# handle the QUIETLY and REQUIRED arguments and set KERBEROS_FOUND to TRUE if 
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(KERBEROS DEFAULT_MSG KERBEROS_LIBRARIES KERBEROS_INCLUDE_DIRS)
+# Note: Must notice the library link orders
+set(KERBEROS_LIBRARIES ${Krb5_LIB_PATH} 
+    ${Com_err_LIB_PATH} ${Krb5_otp_LIB_PATH}
+    ${Krb5_k5tls_LIB_PATH}  ${K5crypto_LIB_PATH} 
+    ${Krb5support_LIB_PATH})
 
 MARK_AS_ADVANCED(KERBEROS_LIBRARIES KERBEROS_INCLUDE_DIRS)
 
